902a6cc43330db0cde5ba885c38c22d06e440d0d
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-04 20:00:29 -0300

    first commit

---
+++ codigo.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+pyautogui.press('win') #Serve para clicar em uma tecla no caso a do windows
+pyautogui.write('Chrome') #Escreve Chrome na barra de pesquisa 
+pyautogui.press('enter') #Serve para dar um enter
