d32508c598560fe8134d6a7ea8c88c8b7c83d83d
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-04 20:44:57 -0300

    Automatizando a abertura da página, achando a posição do mouse e usando o Pandas para ler a base dados do produtos

---
+++ auxiliar.py
@@ -0,0 +1,7 @@
+#Pegando a posição do mouse para o campo de colocar o email
+
+import time
+import pyautogui
+
+time.sleep(5)
+print(pyautogui.position())
--- codigo.py
+++ codigo.py
@@ -1,5 +1,27 @@
+import time
 import pyautogui
 
+pyautogui.PAUSE = 0.5 #Pausando a automação em segundo pois estava sendo muito rápido e o Chrome não conseguia abrir a tempo
+
 pyautogui.press('win') #Serve para clicar em uma tecla no caso a do windows
-pyautogui.write('Chrome') #Escreve Chrome na barra de pesquisa 
+pyautogui.write('chrome') #Escreve Chrome na barra de pesquisa 
 pyautogui.press('enter') #Serve para dar um enter
+
+link = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+pyautogui.write(link)
+pyautogui.press('enter')
+
+time.sleep(3) #Deixando o transição mais de boa
+
+pyautogui.click(x=699, y=377) #Colocando a posição do mouse
+pyautogui.write('emailteste@gmail.com')
+pyautogui.press('tab') #Passando para a cédemailteste@gmail.com senhatesteula de baixo de senha
+pyautogui.write('senhateste')
+
+pyautogui.click(x=952, y=535)
+time.sleep(3)
+
+import pandas #Serve para importar base de dados (no caso a produto.csv)
+tabela = pandas.read_csv('produtos.csv') #Serve para ler o arquivo, como é um csv coloca _csv mas posso colocar read_pdf ou outros
+
+print(tabela)
